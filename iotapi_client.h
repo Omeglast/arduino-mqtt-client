abac2a05ec8764789e837c99e3e59cb88b493905
Jeckel <jeckel@jeckel.fr>
2015-09-03 23:51:56 +0200

    Refactor

--- iotapi_client.h
+++ iotapi_client.h
@@ -17,4 +17,30 @@ Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ
 Adafruit_CC3000_Client client;
 
 
+// Tries to read the IP address and other connection details
+bool displayConnectionDetails(void)
+{
+  uint32_t ipAddress, netmask, gateway, dhcpserv, dnsserv;
+  
+  if(!cc3000.getIPAddress(&ipAddress, &netmask, &gateway, &dhcpserv, &dnsserv))
+  {
+    #ifdef DEBUG
+      Serial.println(F("Unable to retrieve the IP Address!\r\n"));
+    #endif
+    return false;
+  }
+  else
+  {
+    #ifdef DEBUG
+      Serial.print(F("\nIP Addr: ")); cc3000.printIPdotsRev(ipAddress);
+      Serial.print(F("\nNetmask: ")); cc3000.printIPdotsRev(netmask);
+      Serial.print(F("\nGateway: ")); cc3000.printIPdotsRev(gateway);
+      Serial.print(F("\nDHCPsrv: ")); cc3000.printIPdotsRev(dhcpserv);
+      Serial.print(F("\nDNSserv: ")); cc3000.printIPdotsRev(dnsserv);
+      Serial.println();
+    #endif
+    return true;
+  }
+}
+
 #endif
